f904db8b5694a86e6405ad8c7d87f412ae43ae3d
changhong <changhongwang@126.com>
2021-07-14 10:05:36 +0800

    fix: es7

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,12 +3,24 @@
  * @Author: changhong.wang
  * @Date: 2021-07-14 00:42:14
  * @LastEditors: changhong.wang
- * @LastEditTime: 2021-07-14 00:48:25
+ * @LastEditTime: 2021-07-14 09:34:05
  */
 import React from 'react';
+import './index.scss';
+
+const arr = ['Wang', 'Changhong']
 
 export default function Home() {
     return(
-        <div>This is renderred From React!</div>
+        <div>
+            <h3>This is renderred From React!</h3>
+            <ul className="name-list">
+                {
+                    arr.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))
+                }
+            </ul>
+        </div>
     )
 }
