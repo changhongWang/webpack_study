52c7da02b408edb48594d650dcf421891523c6cf
changhong <changhongwang@126.com>
2021-07-26 19:28:10 +0800

    feat: 动态加载

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,20 +5,48 @@
  * @LastEditors: changhong.wang
  * @LastEditTime: 2021-07-14 09:34:05
  */
-import React from "react";
+import React, { Component } from "react";
 import "./index.scss";
 
 const arr = ["Wang", "Changhong"];
 
-export default function Home() {
-  return (
-    <div>
-      <h3 className="title">This is renderred From React!</h3>
-      <ul className="name-list">
-        {arr.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dynamic: "",
+    };
+  }
+
+  dynamicImport() {
+    import("./Components/Dynamic").then((res) => {
+      console.log(787, res);
+      this.setState({
+        dynamic: res.default(),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3 className="title">This is renderred From React!</h3>
+        <button
+          onClick={() => {
+            this.dynamicImport();
+          }}
+        >
+          BTN
+        </button>
+        {this.state.dynamic ? this.state.dynamic : null}
+        <ul className="name-list">
+          {arr.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default Home;
