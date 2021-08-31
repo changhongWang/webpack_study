d23281e9b13a3dd75518e76c3f57367b26c6e9bb
changhong <changhongwang@126.com>
2021-08-31 16:14:27 +0800

    feat: tapable

--- tapable_study/compiler.js
+++ tapable_study/compiler.js
@@ -1,36 +1,40 @@
-const { SyncHook, AsyncSeriesHook } = require("tapable")
+const { SyncHook, AsyncSeriesHook } = require("tapable");
 
 /*
- * @Description: 模拟compiler.js 
+ * @Description: 模拟compiler.js
  * @Author: changhong.wang
  * @Date: 2021-08-31 15:40:00
  * @LastEditors: changhong.wang
- * @LastEditTime: 2021-08-31 15:42:24
+ * @LastEditTime: 2021-08-31 16:12:28
  */
 module.exports = class Compiler {
-    constructor() {
-        this.hooks = {
-            accelerate: new SyncHook(['newSpeed']),
-            brake: new SyncHook(),
-            calculateRoutes: new AsyncSeriesHook(['source', 'target', 'routesList'])
-        }
-    }
-    run() {
-        this.accelerate(10);
-        this.brake();
-        this.calculateRoutes('Async', 'hook', 'demo')
-    }
-    accelerate(speed) {
-        this.hooks.accelerate.call(speed);
-    }
-    brake() {
-        this.hooks.brake.call();
-    }
-    calculateRoutes() {
-        this.hooks.calculateRoutes.promise(...arguments).then(() => {
+  constructor() {
+    this.hooks = {
+      accelerate: new SyncHook(['newSpeed']),
+      brake: new SyncHook(),
+      calculateRoutes: new AsyncSeriesHook(['source', 'target', 'routesList']),
+    };
+  }
 
-        }, err => {
-            console.error(err);
-        })
-    }
+  run() {
+    this.accelerate(10);
+    this.brake();
+    this.calculateRoutes('Async', 'hook', 'demo');
+  }
+
+  accelerate(speed) {
+    this.hooks.accelerate.call(speed);
+  }
+
+  brake() {
+    this.hooks.brake.call();
+  }
+
+  calculateRoutes() {
+    this.hooks.calculateRoutes.promise(...arguments).then(() => {
+
+    }, err => {
+      console.error(err);
+    });
+  }
 }
--- tapable_study/my-plugin.js
+++ tapable_study/my-plugin.js
@@ -3,47 +3,44 @@
  * @Author: changhong.wang
  * @Date: 2021-08-31 15:49:20
  * @LastEditors: changhong.wang
- * @LastEditTime: 2021-08-31 15:55:44
+ * @LastEditTime: 2021-08-31 16:13:52
  */
 const Compiler = require('./compiler');
 
 class MyPlugin {
-    constructor() {
-
-    }
-    apply(compiler) {
-        compiler.hooks.brake.tap('WarningLampPlugin', () => {
-            console.log('WarningLampPlugin')
-        });
-        compiler.hooks.accelerate.tap('LoggerPlugin', newSpeed => {
-            console.log(`Accelerating to ${newSpeed}`)
-        });
-        compiler.hooks.calculateRoutes.tapPromise('calculateRoutes tapAsync', (source, target, routesList) => {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    console.log(`tapPromise to ${source} ${target} ${routesList}`)
-                    resolve();
-                }, 1000)
-            })
-        })
-    }
+  apply(compiler) {
+    compiler.hooks.brake.tap('WarningLampPlugin', () => {
+      console.log('WarningLampPlugin')
+    });
+    compiler.hooks.accelerate.tap('LoggerPlugin', newSpeed => {
+      console.log(`Accelerating to ${newSpeed}`)
+    });
+    compiler.hooks.calculateRoutes.tapPromise('calculateRoutes tapAsync', (source, target, routesList) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log(`tapPromise to ${source} ${target} ${routesList}`)
+          resolve();
+        }, 1000)
+      })
+    })
+  }
 }
 
 // 模拟插件执行
 const myPlugin = new MyPlugin();
 
 const options = {
-    plugins: [myPlugin]
+  plugins: [myPlugin],
 }
 
 const compiler = new Compiler();
 
 for (const plugin of options.plugins) {
-    if (typeof plugin === 'function') {
-        plugin.call(compiler, compiler);
-    } else {
-        plugin.apply(compiler);
-    }
+  if (typeof plugin === 'function') {
+    plugin.call(compiler, compiler);
+  } else {
+    plugin.apply(compiler);
+  }
 }
 
 compiler.run();
